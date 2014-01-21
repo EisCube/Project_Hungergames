36aaeb2f75db5a4e6cbd91456b6bad2ed1af864d
EisCube <simon@hornisch.com>
2014-01-21 21:15:46 +0100

    Firesater are better ;P

---
+++ hungergames_common/com/eiscube/hungergames/items/ItemFirestarter.java
@@ -0,0 +1,25 @@
+package com.eiscube.hungergames.items;
+
+import com.eiscube.hungergames.HungerGames;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.item.Item;
+
+public class ItemFirestarter extends Item {
+
+    public ItemFirestarter(int par1) {
+        super(par1);
+        this.setCreativeTab(HungerGames.tabHungerGames);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IconRegister par1IconRegister)
+    {
+        this.itemIcon = par1IconRegister.registerIcon(HungerGames.modid + ":" + (this.getUnlocalizedName().substring(5)));
+    }
+   
+    //Make it work with Fireplaces
+    
+}
