8775094d58015ac423cbed27157404a2a9386e74
EisCube <simon@hornisch.com>
2014-01-25 16:06:20 +0100

    BlockStick now placable only on dirt and grass

--- hungergames_common/com/eiscube/hungergames/blocks/BlockStick.java
+++ hungergames_common/com/eiscube/hungergames/blocks/BlockStick.java
@@ -4,12 +4,55 @@ import com.eiscube.hungergames.HungerGames;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.world.World;
+import net.minecraftforge.common.EnumPlantType;
+import net.minecraftforge.common.ForgeDirection;
+import net.minecraftforge.common.IPlantable;
 
-public class BlockStick extends Block {
+public class BlockStick extends Block{
 
 	public BlockStick(int id) {
-		super(id,Material.wood);
+		super(id,Material.plants);
 		this.setCreativeTab(HungerGames.tabHungerGames);
 	}
-
+	
+	@Override
+    public int getRenderType()
+    {
+        return 1;
+    }
+	
+    public boolean isOpaqueCube()
+    {
+        return false;
+    }
+    
+    public boolean renderAsNormalBlock()
+    {
+        return false;
+    }
+    
+    public AxisAlignedBB getCollisionBoundingBoxFromPool(World par1World, int par2, int par3, int par4)
+    {
+        return null;
+    }
+    
+    public boolean canPlaceBlockAt(World world,int x,int y,int z){
+        return super.canPlaceBlockAt(world, x, y, z) && canBlockStay(world, x, y, z);
+    }
+    
+    public boolean canBlockStay(World world, int x, int y, int z)
+    {
+    	Block ground = blocksList[world.getBlockId(x, y-1, z)];
+    	
+    	if(ground != null){
+    		if((ground.blockID == Block.grass.blockID || ground.blockID == Block.dirt.blockID)){
+    			return true;
+    		}
+    	}
+    	
+    	return false;
+    	
+    }
 }
